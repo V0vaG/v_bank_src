a71434533d5608bf0fd65d074868c614a381536a
V0vaG <its_a_vio@hotmail.com>
2024-11-21 08:31:22 +0200

    a

--- app/app.py
+++ app/app.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 import os
 import json
-import threading
-import time
 from werkzeug.security import generate_password_hash, check_password_hash
 from functools import wraps
 
